c99ed9f03f293d0d0f9549245f96e3b3b882fb48
Christopher Knauf <chrisknauf@gmail.com>
2020-09-12 21:57:43 -0400

    Test Android App upload
    This android app tests the functionality over BLE, including pause, and early filter  parameters,

---
+++ Android/app/src/main/java/com/knaufinator/motioncompanion/CompanionApplication.java
@@ -0,0 +1,14 @@
+package com.knaufinator.motioncompanion;
+
+import android.app.Application;
+import android.os.Build;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class CompanionApplication  extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+       }
+}
---
+++ Android/app/src/main/java/com/knaufinator/motioncompanion/MainActivity.java
@@ -0,0 +1,68 @@
+package com.knaufinator.motioncompanion;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
